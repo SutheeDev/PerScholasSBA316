02ab4f45c0d73eab7e64708f09eb1fa5685973d5
SU-T <guitaristpeet@gmail.com>
2024-11-23 21:05:30 -0500

    Initial setup

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hey");
+let letterStr =
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis dolore voluptas quaerat, tenetur placeat ipsa ullam iste aliquid, et neque tempora! Natus earum officia voluptate corporis dolores ab reprehenderit! Numquam, minus. Debitis, animi id repellendus in expedita, adipisci ullam molestiae explicabo cumque ducimus modi repellat dolor minima quo, corrupti ipsam temporibus labore voluptatem illum? Suscipit consectetur odio eligendi eveniet fugit doloribus deserunt! Debitis, corporis laboriosam? Deserunt dicta recusandae omnis reiciendis!";
+
+const text = document.getElementById("text");
+const letter = document.createElement("h4");
+letter.textContent = letterStr;
+text.prepend(letter);
+
+document.addEventListener("keydown", (e) => {
+  const char = e.target;
+  console.log(char);
+});
