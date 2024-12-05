19f191aa7e5850a677d7821416ba1d0116aaf533
SU-T <guitaristpeet@gmail.com>
2024-12-05 10:32:30 -0500

    styled difficullty btn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import textToType from "./textToType.js";
 const getTextToType = (level = "easy") => {
   const textArray = textToType.filter((text) => text.level === level);
 
+  // Randomly pick text
   const textIndex = Math.floor(Math.random() * textArray.length);
   const str = textArray[textIndex].str;
   return str;
